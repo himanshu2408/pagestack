08e9dce31fb3dd8484e9247b1c8795da1b1b23de
Nishant Mor <nishantmor27@gmail.com>
2017-03-27 03:28:52 +0530

    API end point to check if the user is authenticated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var api = require('./routes/api');
 
 
 var app = express();
-app.set('port' , process.env.PORT || 3000);
+app.set('port' , process.env.PORT || 80);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
