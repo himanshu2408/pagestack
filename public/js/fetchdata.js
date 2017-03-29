f707d1668ceed7a98199b4941fb03cb8bcc38b50
himanshu2408 <singh.himanshu2408@gmail.com>
2017-03-29 17:13:17 +0530

    modified user-cards

--- public/js/fetchdata.js
+++ public/js/fetchdata.js
@@ -5,12 +5,13 @@ $(function() {
 function updateTable(data) {
     var output= '';
     $.each(data,function(key, item) {
+        var summary = item.summary.substring(0,250);
         output += '<div class="col-md-4 col-sm-6 col-xxs-12">';
-        output += '<div class="thumbnail">';
-        output += '<img src="http://placehold.it/360x125/EEE" class="">';
-        output += '<div class="caption" style="text-align:center">';
-        output += '<h2>'+item.title+'</h2>';
-        output += '<p>'+item.summary+'</p><div class="pull-right" style="cursor:pointer;"><i class="glyphicon glyphicon-remove" id="'+item._id+'"></i></div>';
+        output += '<div class="thumbnail" style="position:relative; height:300px">';
+        output += '<img src="http://placehold.it/750x125/EEE">';
+        output += '<div style="text-align:center">';
+        output += '<h3>'+item.title+'</h3>';
+        output += '<p class="card-text">'+summary+'</p><div class="pull-right" style="cursor:pointer;position:absolute;top:5px;right:8px;"><i class="fa fa-trash-o" id="'+item._id+'"></i></div>';
         output += '</div>';
         output += '</div>';
         output += '</div>';
@@ -20,7 +21,7 @@ function updateTable(data) {
 }
 
 $('.cardslist').on('click', function (e) {
-    if (e.target.className == 'glyphicon glyphicon-remove') {
+    if (e.target.className == 'fa fa-trash-o') {
         console.log(e.target.id);
         $.ajax({
             url: 'api/' + e.target.id,
@@ -31,8 +32,3 @@ $('.cardslist').on('click', function (e) {
 
     }
 });
-
-function myfunc() {
-    console.log("inside myfunc");
-    $.getJSON('api/get_all_user', updateTable);
-}
