186e68a641f36d340e9e9b9220e8305b1fb739df
Himanshu Singh <singh.himanshu2408@gmail.com>
2017-06-23 17:34:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.set('port' , process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
